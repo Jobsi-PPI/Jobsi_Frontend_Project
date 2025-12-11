08542bb1664f625183beb0fa72a8e3cde928a56e
Juanchito1106 <jandrescorrea19@gmail.com>
2025-12-10 22:48:06 -0500

    Ajuste en select de categorias

--- src/features/home/layouts/CreateJobModal.jsx
+++ src/features/home/layouts/CreateJobModal.jsx
@@ -103,11 +103,11 @@ const CreateJobModal = ({
                         value={categoria} onChange={(e) => setCategoria(e.target.value)}
                             className="w-full p-2 border-2 border-[#6b7280] rounded-lg focus:ring-2 focus:ring-blue-500 text-black"
                         >
-                            <option value="1">ASESORIAS</option>
-                            <option value="2">TAREAS</option>
-                            <option value="3">MATERIALES</option>
-                            <option value="4">ENTRENAMIENTOS</option>
-                            <option value="5">OTRO</option>
+                            <option value="ASESORIAS">ASESORIAS</option>
+                            <option value="TAREAS">TAREAS</option>
+                            <option value="MATERIALES">MATERIALES</option>
+                            <option value="ENTRENAMIENTOS">ENTRENAMIENTOS</option>
+                            <option value="OTRO">OTRO</option>
                         </select>
                     </div>
 
